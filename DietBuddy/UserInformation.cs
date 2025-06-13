305d381352052e95c9b7093f235bfe0306b1a81f
CreamCheesePeanutButter <lonelyaraya@gmail.com>
2025-06-13 15:32:36 -0400

    Setting up

---
+++ DietBuddy/UserInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DietBuddy
+{
+	public class UserInformation
+	{
+        // Properties to store user information
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        // Constructor to initialize user information
+        public UserInformation(string name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+    };
+};
+
